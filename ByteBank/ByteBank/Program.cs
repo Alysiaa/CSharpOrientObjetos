292d25370aae3f1b1f25b26ec75a80ebf5dbfbef
Alysiaa <carol_lopees@outlook.com>
2022-12-31 10:53:16 -0300

    Aula 02

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -9,6 +9,20 @@ ContadoAndre.saldo = 100;
 
 Console.WriteLine("Saldo da conta do André: R$ " + ContadoAndre.saldo);
 
+//depositando R$100 na conta do André
+ContadoAndre.Depositar(100);
+Console.WriteLine("Saldo da conta do André pós depósito: R$ " + ContadoAndre.saldo);
+
+//sacando R$100 na conta do André
+if (ContadoAndre.Sacar(100)==true)
+{
+Console.WriteLine("Saldo da conta do André pós saque: R$ " + ContadoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque!");
+}
+
 ContaCorrente ContadoJose = new ContaCorrente();
 
 ContadoJose.titular = "José Gomes";
@@ -16,6 +30,7 @@ ContadoJose.num_agencia = 16;
 ContadoJose.conta = "8594-6";
 ContadoJose.saldo = 503;
 
+Console.WriteLine();
 Console.WriteLine("Saldo da conta do José: R$ " + ContadoJose.saldo);
 
 ContaCorrente ContadaMaria = new ContaCorrente();
@@ -25,4 +40,11 @@ ContadaMaria.num_agencia = 17;
 ContadaMaria.conta = "1018-5";
 ContadaMaria.saldo = 350;
 
+Console.WriteLine();
 Console.WriteLine("Saldo da conta da Maria: R$ " + ContadaMaria.saldo);
+
+//transferindo da conta do André para a conta da Maria
+ContadoAndre.Transferir(50, ContadaMaria);
+
+Console.WriteLine("Saldo do André: R$ "+ContadoAndre.saldo);
+Console.WriteLine("Saldo da Maria: R$ " + ContadaMaria.saldo);
